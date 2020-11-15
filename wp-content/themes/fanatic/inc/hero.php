491b3d784e79a448a2b9c27fa8e0cf312b741cfb
Aliceviccajee <alice@viccajee.com>
2020-11-15 18:36:37 +0000

    Initial commit

---
+++ wp-content/themes/fanatic/inc/hero.php
@@ -0,0 +1,19 @@
+
+<div class="hero">
+  <div class="section"> </div>
+    <?php if( get_field('subheading') ): ?>
+        <h4><?php the_field('subheading'); ?></h4>
+    <?php endif; ?>
+    <?php if( get_field('heading') ): ?>
+        <h2><?php the_field('heading'); ?></h2>
+    <?php endif; ?>
+    <?php if( get_field('paragraph') ): ?>
+        <p><?php the_field('paragraph'); ?></p>
+    <?php endif; ?>
+    <?php 
+      $link = get_field('cta');
+      if( $link ): ?>
+          <a class="button" href="<?php echo esc_url( $link ); ?>">CTA</a>
+    <?php endif; ?>
+  </div>
+</div>
