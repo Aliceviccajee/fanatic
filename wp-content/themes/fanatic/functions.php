dae2cdac932951384a93f9b8d772b40b44b17a2a
Aliceviccajee <alice@viccajee.com>
2020-11-16 12:48:01 +0000

    Hero left styling

--- wp-content/themes/fanatic/functions.php
+++ wp-content/themes/fanatic/functions.php
@@ -345,4 +345,16 @@ function custom_post_type() {
 // Hook into the 'init' action
 add_action( 'init', 'custom_post_type', 0 );
 
+add_action( 'wp_enqueue_scripts', 'themeprefix_slick_enqueue_scripts_styles' );
+// Enqueue Slick scripts and styles
+function themeprefix_slick_enqueue_scripts_styles() {
+
+	wp_enqueue_script( 'slickjs', get_stylesheet_directory_uri() . '/js/slick.min.js', array( 'jquery' ), '1.6.0', true );
+	wp_enqueue_script( 'slickjs-init', get_stylesheet_directory_uri(). '/js/slick-init.js', array( 'slickjs' ), '1.6.0', true );
+
+	wp_enqueue_style( 'slickcss', get_stylesheet_directory_uri() . '/css/slick.css', '1.6.0', 'all');
+	wp_enqueue_style( 'slickcsstheme', get_stylesheet_directory_uri(). '/css/slick-theme.css', '1.6.0', 'all');
+
+}
+
 ?>
--- wp-content/themes/fanatic/inc/hero.php
+++ wp-content/themes/fanatic/inc/hero.php
@@ -1,18 +1,41 @@
 <div class="hero">
-  <div class="section"> </div>
-    <?php if( get_field('subheading') ): ?>
-        <h4><?php the_field('subheading'); ?></h4>
-    <?php endif; ?>
-    <?php if( get_field('heading') ): ?>
-        <h2><?php the_field('heading'); ?></h2>
-    <?php endif; ?>
-    <?php if( get_field('paragraph') ): ?>
-        <p><?php the_field('paragraph'); ?></p>
-    <?php endif; ?>
-    <?php 
-      $link = get_field('cta');
-      if( $link ): ?>
-          <a class="button" href="<?php echo esc_url( $link ); ?>">CTA</a>
-    <?php endif; ?>
+  <div class="hero-left">
+    <div class="hero-text">
+      <?php if( get_field('subheading') ): ?>
+          <h4 class="sub-heading"><?php the_field('subheading'); ?></h4>
+      <?php endif; ?>
+      <?php if( get_field('heading') ): ?>
+          <h2><?php the_field('heading'); ?></h2>
+      <?php endif; ?>
+      <?php if( get_field('paragraph') ): ?>
+          <p><?php the_field('paragraph'); ?></p>
+      <?php endif; ?>
+      <?php 
+        $link = get_field('cta');
+        if( $link ): ?>
+            <a class="hero-cta" href="<?php echo esc_url( $link ); ?>">CTA Button</a>
+      <?php endif; ?>
+    </div>
   </div>
-</div>
+  
+  <div class="hero-right slider">
+  <?php 
+    $images = get_field('slider_portfolio');
+    if( $images ): ?>
+    <div class="slider-for">
+      <?php foreach( $images as $image ): ?>
+      <div class="slick-container">
+        <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
+      </div>
+      <?php endforeach; ?>
+      </div>
+      <div class="slider-nav">
+        <?php foreach( $images as $image ): ?>
+        <div>
+          <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
+        </div>
+        <?php endforeach; ?>
+      </div>
+     </div>
+  </div>
+<?php endif; 
