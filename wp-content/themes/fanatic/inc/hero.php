0277e9ce772d2a86e3e5c5287bbdcb75d6317499
Aliceviccajee <alice@viccajee.com>
2020-11-17 20:16:15 +0000

    hero responsive

--- wp-content/themes/fanatic/inc/hero.php
+++ wp-content/themes/fanatic/inc/hero.php
@@ -2,8 +2,8 @@
   <div class="hero-left">
     <div class="hero-text">
       <div class="headings">
-        <?php if( get_field('subheading') ): ?>
-            <h4 class="sub-heading"><?php the_field('subheading'); ?></h4>
+        <?php if( get_field('sub_header') ): ?>
+            <h4 class="sub-header"><?php the_field('sub_header'); ?></h4>
         <?php endif; ?>
         <?php if( get_field('heading') ): ?>
             <h2><?php the_field('heading'); ?></h2>
@@ -14,7 +14,7 @@
       <?php endif; ?>
       <div class="button"> 
         <?php 
-          $link = get_field('cta');
+          $link = get_field('cta_button');
           if( $link ): ?>
               <a class="hero-cta" href="<?php echo esc_url( $link ); ?>">CTA Button</a>
         <?php endif; ?>
@@ -22,6 +22,25 @@
     </div>
   </div>
   <div class="hero-right">
-    <?php echo do_shortcode('[metaslider id="49"]'); ?>
+  <?php  
+  $images = get_field('gallery');
+    if( $images ): ?>
+    <div class="slider-for">
+      <?php foreach( $images as $image ): ?>
+      <div class="slick-container">
+        <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
+      </div>
+      <?php endforeach; ?>
+    </div>
+    <div class="slider-nav">
+      <?php foreach( $images as $image ): ?>
+      <div>
+      <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
+    </div>
+    <?php endforeach; ?>
+    </div>
+    </div>
   </div>
-</div>
+
+
+<?php endif; 
--- wp-content/themes/fanatic/js/functions.js
+++ wp-content/themes/fanatic/js/functions.js
@@ -11,39 +11,3 @@ $(window).resize(function() { // Hide Mobile Menu if Browser window goes above 7
     $('.search_form').hide();
   }
 });
-
-$('.slick-slider').slick({
-  dots: true,
-  infinite: true,
-  speed: 300,
-  slidesToShow: 3,
-  slidesToScroll: 3,
-  responsive: [
-    {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        infinite: true,
-        dots: true
-      }
-    },
-    {
-      breakpoint: 800,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 2
-      }
-    },
-    {
-      breakpoint: 680,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1
-      }
-    }
-    // You can unslick at a given breakpoint now by adding:
-    // settings: "unslick"
-    // instead of a settings object
-  ]
-});
