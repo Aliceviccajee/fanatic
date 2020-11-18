3f3ba1905ff79d6637bba0a1048a22360d3d8f6e
Aliceviccajee <alice@viccajee.com>
2020-11-18 15:01:37 +0000

    File path fix

--- wp-content/themes/fanatic/functions.php
+++ wp-content/themes/fanatic/functions.php
@@ -357,11 +357,11 @@ add_action( 'wp_enqueue_scripts', 'themeprefix_slick_enqueue_scripts_styles' );
 // Enqueue Slick scripts and styles
 function themeprefix_slick_enqueue_scripts_styles() {
 
-	wp_enqueue_script( 'slickjs', get_stylesheet_directory_uri() . '/js/slick.min.js', array( 'jquery' ), '1.6.0', true );
-	wp_enqueue_script( 'slickjs-init', get_stylesheet_directory_uri(). '/js/slick-init.js', array( 'slickjs' ), '1.6.0', true );
+	wp_enqueue_script( 'slickjs', get_stylesheet_directory_uri() . '/js/vendor/slick.min.js', array( 'jquery' ), '1.6.0', true );
+	wp_enqueue_script( 'slickjs-init', get_stylesheet_directory_uri(). '/js/vendor/slick-init.js', array( 'slickjs' ), '1.6.0', true );
 
-	wp_enqueue_style( 'slickcss', get_stylesheet_directory_uri() . '/css/slick.css', '1.6.0', 'all');
-	wp_enqueue_style( 'slickcsstheme', get_stylesheet_directory_uri(). '/css/slick-theme.css', '1.6.0', 'all');
+	wp_enqueue_style( 'slickcss', get_stylesheet_directory_uri() . '/css/vendor/slick.css', '1.6.0', 'all');
+	wp_enqueue_style( 'slickcsstheme', get_stylesheet_directory_uri(). '/css/vendor/slick-theme.css', '1.6.0', 'all');
 
 }
 
